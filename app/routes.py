d62bf97361c218afc24b64a5a9da8e66c9fc25cc
Chris Maikos <chmaikos@gmail.com>
2023-04-09 20:28:57 +0300

    reset

--- app/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template, request
-from . import app
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        # TODO: Handle login form submission
-        pass
-    return render_template('login.html')
